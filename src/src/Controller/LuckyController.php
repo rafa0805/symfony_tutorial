a61bb27fd0c5d77293bdf7a411ad14fc1230a941
rafa0805 <theryk0805@gmail.com>
2022-08-11 20:54:32 +0900

    Max value of random number given dynamically by route parameter

--- src/src/Controller/LuckyController.php
+++ src/src/Controller/LuckyController.php
@@ -1,19 +1,23 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-    #[Route('/lucky/number')]
-    public function number(): Response
+    #[Route('/lucky/number/{max<\d+>?100}', name: 'app_lucky_number')]
+    public function number(int $max, LoggerInterface $logger): Response
     {
-        $number = random_int(0, 100);
+        $number = random_int(0, $max);
+        $nextLink = $this->generateUrl('app_lucky_number', ['max' => $max]);
+        $logger->info("random number was $number");
 
         return $this->render('lucky/number.html.twig', [
-            'number' => $number
+            'number' => $number,
+            'next_link' => $nextLink,
         ]);
     }
 }
