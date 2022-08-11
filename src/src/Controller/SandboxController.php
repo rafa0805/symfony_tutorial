949700362e121c15e4ed4b585e995dbff089a6bf
rafa0805 <theryk0805@gmail.com>
2022-08-11 21:44:11 +0900

    Play around with SessionInterface

--- src/src/Controller/SandboxController.php
+++ src/src/Controller/SandboxController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SandboxController extends AbstractController
@@ -26,6 +27,18 @@ class SandboxController extends AbstractController
     return $this->render('sandbox/xss_test_ng.html.twig', [
       'text' => $injectTxt,
     ]);
+  }
 
+  #[Route('/session_test', name: 'session_test_page')]
+  public function session_test(SessionInterface $session): Response
+  {
+    $current_count = !$session->get('count_up') ? 0 : $session->get('count_up');
+    $current_count++;
+    $session->set('count_up', $current_count);
+
+    return $this->render('sandbox/session_test.html.twig', [
+      'sess_id' => $session->getId(),
+      'count' => $session->get('count_up'),
+    ]);
   }
 }
